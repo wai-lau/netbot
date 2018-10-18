fa02e10dcb2451253ad9630b115e1614a8b8ff9b
Wai-Lau <wl.wailau@gmail.com>
2018-10-17 21:49:51 -0400

    Add simple way to create programs

--- app/models/grid/program.rb
+++ app/models/grid/program.rb
@@ -4,15 +4,48 @@ class Grid
     attr_accessor :color
     attr_accessor :max_size
     attr_accessor :sector_list
-    attr_accessor :move
+    attr_accessor :max_move
+    attr_accessor :cur_move
 
-    def current_size
+    def cur_size
       if @sector_list.nil?
         return 0
       else
         return @sector_list.length
       end
     end
+
+    def initialize(name_symbol)
+      if PROGRAM_LIST[name_symbol]
+        program_data = PROGRAM_LIST[name_symbol]
+        @name = if program_data[:name]
+                  program_data[:name]
+                else
+                  name_symbol.to_s.capitalize
+                end
+        @color = program_data[:color]
+        @max_size = program_data[:max_size]
+        @max_move = program_data[:max_move]
+        @cur_move = program_data[:max_move]
+      end
+    end
+
+    private
+
+    PROGRAM_LIST = {
+      hack: {
+        name: "Hack 1.0",
+        color: "Blue",
+        max_size: 4,
+        max_move: 2
+      },
+
+      slingshot: {
+        color: "Teal",
+        max_size: 3,
+        max_move: 2
+      }
+    }
   end
 end
 
--- test/models/grid/program_test.rb
+++ test/models/grid/program_test.rb
@@ -2,9 +2,16 @@ require 'minitest/autorun'
 
 class GridTest
   class ProgramTest < MiniTest::Test
-    def test_example 
-      program = Grid::Program.new
-      assert !program.nil?
+    def test_program_can_create_hack 
+      program = Grid::Program.new(:hack)
+      assert_equal program.name, "Hack 1.0"
+      assert_equal program.max_size, 4
+    end
+    
+    def test_program_can_create_slingshot 
+      program = Grid::Program.new(:slingshot)
+      assert_equal program.name, "Slingshot"
+      assert_equal program.max_size, 3
     end
   end
 end
