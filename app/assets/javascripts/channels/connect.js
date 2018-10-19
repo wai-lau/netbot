ecc10f99b9b88f0e09072fb04eb0093d3df37956
Wai-Lau <wl.wailau@gmail.com>
2018-10-19 11:22:26 -0400

    Refactor js structure, add keypress file

--- app/assets/javascripts/channels/connect.js
+++ app/assets/javascripts/channels/connect.js
@@ -1,31 +1,34 @@
-function connect() {
+connect = () => {
   App.moves = App.cable.subscriptions.create('MovesChannel', {  
-    received: function(data) {
-      $('#move-input').val(''); 
-      $('#moves').prepend(this.textResponse(data.text));
+    received: (data) => {
+      $('#move-input').val('');
+      if (data.text) this.textResponse(data.text); 
       if (data.update) this.renderGrid(data.grid_state);
       return
-    },
-    textResponse: function(text) {
-      return "<p>" + text + "</p>";
-    },
-    renderGrid: function(grid_state) {
-      var newGrid = ""
-      gs = grid_state
-      grid_state.forEach( function(row) {
-	row.forEach( function(tile) {
-          newGrid +=
-	    `
-	    <div style="background-color: ${tile.owner && tile.owner.color ? tile.owner.color : 'light-grey; opacity: 0.1'}" class="tile">
-	      <p>
-	        ${tile.head && tile.owner && tile.owner.name[0] ? tile.owner.name[0] : ""}
-	      </p>
-	    </div>
-	    `
-	});
-      });
-      $("#game-grid").css("grid-template-areas","'" + "a ".repeat(grid_state[0].length) + "'")
-      $("#game-grid").html(newGrid);
     }
   });
 }
+
+textResponse = (text) => {
+  $('#moves').prepend("<p>" + text + "</p>");
+}
+	
+renderGrid = (grid_state) => {
+  let newGrid = ""
+  gs = grid_state
+  grid_state.forEach( (row) => {
+  row.forEach( (tile) => {
+    newGrid +=
+      `
+      <div style="background-color: ${tile.owner && tile.owner.color ? tile.owner.color : 'light-grey; opacity: 0.1'}" class="tile">
+        <p>
+  	  ${tile.head && tile.owner && tile.owner.name[0] ? tile.owner.name[0] : ""}
+        </p>
+      </div>
+      `
+    });
+  });
+  $("#game-grid").css("grid-template-areas","'" + "a ".repeat(grid_state[0].length) + "'")
+  $("#game-grid").html(newGrid);
+}
+
