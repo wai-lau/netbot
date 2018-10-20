3f8801bc263c8470bed28c2f98a565a09eaa67e2
Wai-Lau <wl.wailau@gmail.com>
2018-10-19 21:35:39 -0400

    Make request when there is a keypress #3

--- app/assets/javascripts/channels/keypress.js
+++ app/assets/javascripts/channels/keypress.js
@@ -1,1 +1,12 @@
-keypress = () => {}
+keypress = (url) => {
+  $(document).keypress( (event) => {
+     $.post( url, {
+       "move": {
+         "content": String.fromCharCode(event.keyCode)
+       }
+     })
+  });
+  $("#move-input").keypress ( (event) => {
+    event.stopPropagation();
+  });
+}
